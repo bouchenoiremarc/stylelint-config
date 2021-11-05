d5d32fbdaae6fcb6d5bcd45310ff193b391a5935
Marc Bouchenoire <bouchenoire.marc@sfr.fr>
2021-11-05 12:13:09 +0100

    Create initial commit

---
+++ scss.js
@@ -0,0 +1,14 @@
+module.exports = {
+  plugins: ["stylelint-scss"],
+  extends: [
+    "stylelint-config-standard-scss",
+    "stylelint-config-css-modules",
+    "stylelint-config-idiomatic-order",
+    "stylelint-config-prettier"
+  ],
+  defaultSeverity: "warning",
+  rules: {
+    "at-rule-no-unknown": null,
+    "scss/at-rule-no-unknown": true
+  }
+}
